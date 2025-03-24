1520787d7a6ad945563b0de12e69ee6841d8bae8
gorejkoff <neskazua63@gmail.com>
2025-03-24 16:43:33 +0300

    change path

--- src/js/modules_js/_babylon.js
+++ src/js/modules_js/_babylon.js
@@ -4,11 +4,11 @@ window.addEventListener('DOMContentLoaded', function () {
    let cylinder;
    let viewportX;
    let viewportY;
-   let canvasSiseX;
-   let canvasSiseY;
+   // let canvasSiseX;
+   // let canvasSiseY;
    function addSizeViewport() {
-      canvasSiseX = window.innerWidth;
-      canvasSiseY = window.innerHeight + 100;
+      // canvasSiseX = window.innerWidth;
+      // canvasSiseY = window.innerHeight + 100;
       viewportX = Math.min(window.innerWidth, 1440);
       viewportY = window.innerHeight;
    }
@@ -20,17 +20,12 @@ window.addEventListener('DOMContentLoaded', function () {
    // Инициализация движка
    const canvas = document.getElementById("container3D");
    const engine = new BABYLON.Engine(canvas, true);
-   // engine.loadingScreen = null;
    // Создание сцены
-
    const scene = new BABYLON.Scene(engine);
-   // scene.stopAllAnimations();
    scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
    // Добавление камеры
    let distance = 40;
-   if (MIN768.matches) {
-      distance = 20;
-   }
+   if (MIN768.matches) { distance = 20 };
    const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2, distance, BABYLON.Vector3.Zero(), scene);
    camera.attachControl(canvas, true);
 
@@ -39,33 +34,19 @@ window.addEventListener('DOMContentLoaded', function () {
 
    // Загрузка модели
    // BABYLON.SceneLoader.Append("../glb/", "transmitter-2.glb", scene, function (scene) {
-   BABYLON.SceneLoader.Append("../dist/glb/transmitter-2.glb", "transmitter-2.glb", scene, function (scene) {
-      // console.log("Модель загружена!");
+   BABYLON.SceneLoader.Append("https://gorejkoff.github.io/Ural_factories/dist/glb/", "transmitter-2.glb", scene, function (scene) {
       scene.animationGroups.forEach(animationGroup => {
-         animationGroup.stop(); // Остановка анимации
-         animationGroup.dispose(); // Удаление анимации из памяти
+         animationGroup.stop();
+         animationGroup.dispose();
       });
-      // console.log("Все анимации удалены!");
-      // список имен
-      // const models = scene.meshes;
-      // console.log(models);
-      // models.forEach(mesh => console.log(mesh.name));
-
       model = scene.getMeshByName("__root__");
-
-      // model.position = new BABYLON.Vector3(0, 0, 0);
       if (MIN768.matches) {
          model.position = new BABYLON.Vector3(0, -3, 0);
       }
       cylinder = model._children[0];
-      // const empty = model.getChildMeshes().find((mesh) => { mesh.name === "Empty" });
       empty = model._children[1];
-      // console.log(empty);
-
       empty.rotation = new BABYLON.Vector3(0, 0, 0)
-      // empty.position = new BABYLON.Vector3(0, 0, 0)
-
-      // console.log(empty);
+      console.log(cylinder);
 
    });
 
