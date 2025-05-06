d76255d53684bd8d2005c64a9b418aa2e9627a7f
gorejkoff <neskazua63@gmail.com>
2025-05-07 00:21:18 +0300

    adit 3d

--- src/js/modules_js/_babylon.js
+++ src/js/modules_js/_babylon.js
@@ -68,10 +68,10 @@ window.addEventListener('DOMContentLoaded', function () {
    //todo--- Добавление света 
 
    const light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(-1, 1, 5), scene);
-   light1.intensity = 1;
+   light1.intensity = 0.2;
    light1.diffuse = new BABYLON.Color3(1, 1, 1);
    const light2 = new BABYLON.HemisphericLight("light2", new BABYLON.Vector3(0, 0, -5), scene);
-   light2.intensity = 0.3;
+   light2.intensity = 0.1;
    light2.diffuse = new BABYLON.Color3(1, 1, 1);
 
 
@@ -110,13 +110,13 @@ window.addEventListener('DOMContentLoaded', function () {
    // );
 
    // Загрузка модели
-   BABYLON.SceneLoader.Append("https://gorejkoff.github.io/Ural_factories/dist/glb/", "edit-4.glb", scene, function (scene) {
-      // BABYLON.SceneLoader.Append("../glb/", "radio-texture-1.glb", scene, function (scene) {
-      // BABYLON.SceneLoader.Append("../glb/", "edit-4.glb", scene, function (scene) {
-      scene.animationGroups.forEach(animationGroup => {
-         animationGroup.stop();
-         animationGroup.dispose();
-      });
+   // BABYLON.SceneLoader.Append("https://gorejkoff.github.io/Ural_factories/dist/glb/", "edit-4.glb", scene, function (scene) {
+   // BABYLON.SceneLoader.Append("../glb/", "radio-texture-1.glb", scene, function (scene) {
+   BABYLON.SceneLoader.Append("../glb/", "edit-7.glb", scene, function (scene) {
+      // scene.animationGroups.forEach(animationGroup => {
+      //    animationGroup.stop();
+      //    animationGroup.dispose();
+      // });
 
       model = scene.getMeshByName("__root__");
       if (!model) {
@@ -188,14 +188,14 @@ window.addEventListener('DOMContentLoaded', function () {
       // console.log(lights, " - все источники света");
       //todo--- Добавление света 
       light_screen = scene.getLightByName("light_screen");
-      light_screen.intensity = 20;
-      light_screen.diffuse = new BABYLON.Color3(1, 0.065, 0);
-      light_screen.range = 3000;
-
+      // light_screen.intensity = 8;
+      // light_screen.position.y = -3.5;
+      // light_screen.range = 5;
+      // light_screen.diffuse = new BABYLON.Color3(1, 0.065, 0);
       // console.log(light_screen, " - light_screen");
 
       light_side = scene.getLightByName("light_side");
-      // light_side.intensity = 2000;
+      // light_side.intensity = 1500;
       light_side.shadowEnabled = true;
       console.log(light_side, " - light_side");
 
@@ -207,6 +207,10 @@ window.addEventListener('DOMContentLoaded', function () {
       shadowGenerator.addShadowCaster(mesh_primitive0, front_frame);
       // свет игнорирует указанные модели
       light_side.excludedMeshes.push(front_frame, glass[0]);
+
+
+
+
 
 
       // группировка радиостации и боковой подсветки
@@ -328,6 +332,8 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
    });
+
+
 
    //!!! Inspector 
    // scene.debugLayer.show();
